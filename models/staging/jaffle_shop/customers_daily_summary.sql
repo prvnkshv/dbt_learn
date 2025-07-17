bf6e2020bbb2b9b0d111e10021e70895bfe4bdee
Biosurfer PK <praveenkeshav707@gmail.com>
2025-07-17 11:25:28 +0000

    code updates related to jina, macros and packages

---
+++ models/staging/jaffle_shop/customers_daily_summary.sql
@@ -0,0 +1,11 @@
+--packages with macros
+
+select
+    
+    {{ dbt_utils.generate_surrogate_key(['customer_id', 'order_date']) }} as id,
+    customer_id,
+    order_date,
+    count(*) as num_orders
+
+from {{ ref('stg_jaffle_shop__orders') }}
+group by 1,2,3
