b254e2030dfd30e040085d985284715a950d4aa9
Biosurfer PK <praveenkeshav707@gmail.com>
2025-07-18 08:31:33 +0000

    snapshot code for raw.jaffle_shop.order and raw.stripe.payment updated

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,9 +1,10 @@
+{{ limit_data_in_dev(column_name='order_date', dev_days_of_data=1000) }}
+
 select
         id as order_id,
         user_id as customer_id,
         order_date,
         status
 
-    from {{ source('jaffle_shop', 'orders') }}
+from {{ source('jaffle_shop', 'orders') }}
 
-    {{ limit_data_in_dev(column_name='order_date', dev_days_of_data=1000) }}
