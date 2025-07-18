a1bf2e933969f19590a29771cbb0843e927da8a5
Biosurfer PK <praveenkeshav707@gmail.com>
2025-07-18 19:53:57 +0000

    few updates

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,11 +1,22 @@
+with deduped as (
+    select *
+    from (
+        select *,
+            row_number() over (
+                partition by id
+                order by _batched_at desc
+            ) as row_num
+        from {{ source('stripe', 'payment') }}
+    )
+    where row_num = 1
+)
+
 select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-
-    -- amount is stored in cents, convert it to dollars
     {{ cents_to_dollars('amount', 4) }} as amount,
-    created as created_at
-
-from {{ source('stripe', 'payment') }}
+    created as created_at,
+    _batched_at
+from deduped
