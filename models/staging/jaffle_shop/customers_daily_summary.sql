62f67c36375bcc7d8aebff16fa84aec87f2a0e31
hancfgnnktccchbmjw@nespf.com <hancfgnnktccchbmjw@nespf.com>
2025-10-05 20:51:17 +0000

    tests configurations are updated for staging and mart models

--- models/staging/jaffle_shop/customers_daily_summary.sql
+++ models/staging/jaffle_shop/customers_daily_summary.sql
@@ -1,5 +1,7 @@
 -- This overrides the config in dbt_project.yml, and this model will not require tests
-{{ config(required_tests=None) }}
+{{ config(
+    meta={'required_tests': None}
+) }}
 
 select
     
--- models/union_tables_by_orders_prefix.sql
+++
@@ -1,7 +0,0 @@
--- This overrides the config in dbt_project.yml, and this model will not require tests
-{{ config(required_tests=None) }}
-
-select * 
-from (
-    {{ union_tables_by_prefix('raw', 'dbt_jinja', 'orders__') }}
-)
